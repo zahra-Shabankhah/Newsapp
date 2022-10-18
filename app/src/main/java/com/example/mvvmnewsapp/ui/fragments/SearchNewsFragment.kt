525f2a2da8c690604cdb05227f27bc0bf7276abf
Zahra Shabankhah <za.sh.dev@gmail.com>
2022-10-18 11:05:23 +0200

    First setup and add BottomNavigation

---
+++ app/src/main/java/com/example/mvvmnewsapp/ui/fragments/SearchNewsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.mvvmnewsapp.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.mvvmnewsapp.R
+
+class SearchNewsFragment : Fragment(R.layout.fragment_search_news) {
+
+}
