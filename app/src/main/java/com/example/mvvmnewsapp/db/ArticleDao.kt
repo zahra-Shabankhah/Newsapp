881aa29fa4411bc0c2323897ac8c028453ef8b4b
Zahra Shabankhah <za.sh.dev@gmail.com>
2022-10-24 09:50:33 +0200

    DAO and setup ROOM

---
+++ app/src/main/java/com/example/mvvmnewsapp/db/ArticleDao.kt
@@ -0,0 +1,23 @@
+package com.example.mvvmnewsapp.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long   // long = which id that inserted
+
+    @Query("SELECT * FROM articles")
+    fun getAllData(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+
+}
